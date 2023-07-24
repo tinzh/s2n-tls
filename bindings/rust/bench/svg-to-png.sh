5cdc7ff6a0300bc373d110a622d4371266205a21
tinzh <jmzhang@amazon.com>
2023-07-24 18:58:39 +0000

    update rustls aws-lc-rs logic to have correct patch

--- bindings/rust/bench/svg-to-png.sh
+++ bindings/rust/bench/svg-to-png.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+# needs: imagemagick, librsvg2-bin
+
 set -e
 
-if [ $# -eq 0 ]
-then
-    echo "need filepath"
-    exit 1
-fi
-
-
-pushd "$1" > /dev/null
+pushd "$(dirname "$0")" > /dev/null
+cd target/criterion
 
 mkdir -p png
 
 for svg_path in `find . | grep report/violin.svg`
 do
+    bench_group_name="$(echo "$svg_path" | sed 's|.\/|| ; s|\/report\/violin.svg||')"
+    sed "s|$bench_group_name\/|| ; s|Input||" "$svg_path" > temp.svg
     flat_png_name=png/"$(echo $svg_path | sed 's|.\/|| ; s|\/report\/violin|| ; s|\/|-|g ; s|svg|png|')"
-    rsvg-convert -h 1000 $svg_path > $flat_png_name
-    convert $flat_png_name -crop 3550x760+1700+140 $flat_png_name
-    convert $flat_png_name -trim $flat_png_name
+    rsvg-convert -h 1000 temp.svg > "$flat_png_name"
+    convert "$flat_png_name" -trim "$flat_png_name"
 done
+
+popd
--- bindings/rust/bench/use-awslc-rustls.sh
+++ bindings/rust/bench/use-awslc-rustls.sh
@@ -16,21 +16,32 @@ rm -rf target/rustls
 git clone https://github.com/rustls/rustls target/rustls
 cd target/rustls
 git checkout 'v/0.21.5'
+cd ../..
+
+# clone aws-lc-rs to bench/target/aws-lc-rs
+rm -rf target/aws-lc-rs
+git clone https://github.com/aws/aws-lc-rs target/aws-lc-rs
+cd target/aws-lc-rs/aws-lc-rs
+git submodule init
+git submodule update
+
+# change aws-lc-rs to look like ring
+sed -i 's|name = .*|name = "ring"| ; s|version = "1.2.1"|version = "0.16.20"|' Cargo.toml
 
 # go to dir with rustls crate
-cd rustls
-rustls_dir="$(pwd)"
+cd ../../rustls
+rustls_dir="$(pwd)"/rustls
 
-# change rustls to use aws-lc-rs
-sed -i 's|ring = .*|ring = { package = "aws-lc-rs" }|' Cargo.toml
-
-# tell Cargo to use custom rustls
+# tell Cargo to use custom rustls and aws-lc-rs
 cd $bench_dir
 mkdir -p .cargo
 # if .cargo/config.toml doesn't already have an [patch.crates-io] header, add it
 if [[ ! -f .cargo/config.toml || "$(cat .cargo/config.toml)" != *"[patch.crates-io]"* ]]; then
 echo "[patch.crates-io]
-rustls = { path = \"$rustls_dir\" }" >> .cargo/config.toml
+rustls = { path = \"$rustls_dir\" }
+ring = { path = \"target/aws-lc-rs/aws-lc-rs\" }" >> .cargo/config.toml
 fi
+
+rm -rf target/release
 
 popd > /dev/null
